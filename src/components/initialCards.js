44ac9cb92123c8c129841faf311156117afe7be9
Daria Takush <dashatakush@yandex.ru>
2021-04-21 18:11:45 +0300

    Добавлен Webpack

---
+++ src/components/initialCards.js
@@ -0,0 +1,33 @@
+import barcelona from '../images/barcelona.jpg';
+import georgia from '../images/georgia.jpg';
+import london from '../images/london.jpg';
+import nhatrang from '../images/nha-trang.jpg';
+import norway from '../images/norway.jpg';
+import rome from '../images/rome.jpg';
+
+export const initialCards = [
+    {
+        name: 'Испания',
+        link: barcelona
+    },
+    {
+        name: 'Грузия',
+        link: georgia
+    },
+    {
+        name: 'Великобритания',
+        link: london
+    },
+    {
+        name: 'Вьетнам',
+        link: nhatrang
+    },
+    {
+        name: 'Норвегия',
+        link: norway
+    },
+    {
+        name: 'Италия',
+        link: rome
+    }
+];
---
+++ webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path'); // подключаем path к конфигу вебпак
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // подключаем плагин для работы с html
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); // подключили плагин для очищения dist
+const MiniCssExtractPlugin = require('mini-css-extract-plugin'); 
+
+
+module.exports = {
+    //Откуда начать
+    entry: { main: './src/pages/index.js' },
+
+    //Куда сохранять
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js',
+                publicPath: ''
+    },
+    mode: 'development', // добавили режим разработчика
+
+    devServer: {
+        contentBase: path.resolve(__dirname, './dist'), // путь, куда "смотрит" режим разработчика
+        compress: true, // это ускорит загрузку в режиме разработки
+        port: 8080, // порт, чтобы открывать сайт по адресу localhost:8080, но можно поменять порт
+        open: true // сайт будет открываться сам при запуске npm run dev
+      },
+
+    module: {
+        rules: [ // rules — это массив правил
+          // добавим в него объект правил для бабеля
+          {
+            // ищет все js файлы
+            test: /\.js$/,
+            // при обработке этих файлов нужно использовать babel-loader
+            use: 'babel-loader',
+            // исключает папку node_modules, файлы в ней обрабатывать не нужно
+            exclude: '/node_modules/'
+          },
+
+          // добавили правило для обработки файлов
+          {
+            // регулярное выражение, которое ищет все файлы с такими расширениями
+            test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
+            type: 'asset/resource',
+            /*use: [{
+                loader: 'file-loader',
+                options: {
+                    esModule: false
+                }
+            }]*/
+            
+          },
+
+          {
+            // применять это правило только к CSS-файлам
+            test: /\.css$/,
+            // при обработке этих файлов нужно использовать
+            // MiniCssExtractPlugin.loader и css-loader
+            use: [MiniCssExtractPlugin.loader, {
+              loader: 'css-loader',
+              options: { importLoaders: 1 }
+            },
+              // добавили postcss-loader
+            'postcss-loader']
+          }
+        ],
+      },
+
+    //вызов плагинов
+    plugins: [
+        new HtmlWebpackPlugin({
+          template: './src/index.html' // путь к файлу index.html
+        }),
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+    ] 
+}
